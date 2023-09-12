e731ab85e318cce72b31ccd4477299daeeeeb81b
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2023-09-13 00:14:37 +0900

    Add files via upload

---
+++ 4470.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int n, i;
+	cin >> n;
+	string* s = new string[n]();
+
+	cin.ignore();
+	for (i = 0; i < n; i++) {
+		getline(cin, s[i]);
+	}
+
+	for (i = 0; i < n; i++)
+		cout << i + 1 << ". " << s[i] << endl;
+
+	return 0;
+}
