f2da239bcf2628981af3685fed71572a4ee40f1b
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2023-09-13 22:58:37 +0900

    Add files via upload

---
+++ 10101.cpp
@@ -0,0 +1,22 @@
+// 삼각형 외우기
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	if ((a == 60) && (b == 60) && (c == 60))
+		cout << "Equilateral";
+	else if (a + b + c == 180) {
+		if ((a == b) || (b == c) || (a == c))
+			cout << "Isosceles";
+		else if ((a != b) && (b != c) && (a != c))
+			cout << "Scalene";
+	}
+	else
+		cout << "Error";
+
+	return 0;
+}
