4c6599eedcdf067e949ca4deee124f594e38fc72
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2023-09-16 23:09:56 +0900

    Update 28431.cpp

--- 28431.cpp
+++ 28431.cpp
@@ -1,30 +1,53 @@
-// ¾ç¸» Â¦ ¸ÂÃß±â
+// ì–‘ë§ ì§ ë§ì¶”ê¸°
 #include <iostream>
 using namespace std;
 
 int main() {
-	int i, sock[5];
+	int i, sock[5], cnt = 0, tmp[3], res[3] = { 0 };
 
-	for (i = 0; i < 5; i++)
+	for (i = 0; i < 5; i++) 
 		cin >> sock[i];
-	
-	if (sock[0] == sock[1]) {
-		if (sock[0] == sock[2])
-			cout << sock[0];
-		else if (sock[2] == sock[3])
-			cout << sock[4];
-		else if (sock[2] == sock[4])
-			cout << sock[3];
-		else
-			cout << sock[2];
+
+	tmp[0] = sock[0];
+
+	for (i = 1; i < 5; i++) {
+		if (tmp[0] != sock[i])
+			cnt++;
 	}
-	else if (sock[0] == sock[2])
-		if (sock[0] == sock[3])
-			cout << sock[0];
-		else if (sock[1] == sock[3])
-			cout << sock[4];
-
-
-	
+	if (cnt == 2) {
+		cnt = 0;
+		for (i = 1; i < 5; i++) {
+			if (tmp[0] != sock[i]) {
+				tmp[1] = sock[i];
+				cnt++;
+			}
+		}
+		if (cnt == 2)
+			cout << tmp[0];
+		else
+			cout << tmp[1];
+	}
+	else {
+		for (i = 1; i < 5; i++) {
+			if (tmp[1] != sock[i]) 
+				tmp[1] = sock[i];
+		}
+		for (i = 1; i < 5; i++) {
+			if ((tmp[0] != sock[i]) && (tmp[1] != sock[i]))
+				tmp[2] = sock[i];
+		}
+		for (i = 0; i < 5; i++) {
+			if (tmp[0] == sock[i])
+				res[0]++;
+			else if (tmp[1] == sock[i])
+				res[1]++;
+			else
+				res[2]++;
+		}
+		for (i = 0; i < 3; i++) {
+			if (res[i] == 1)
+				cout << tmp[i];
+		}
+	}
 	return 0;
 }
