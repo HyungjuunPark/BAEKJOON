6de6f4d95e425a0303ed0965c52906eb78fb48c3
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2023-09-18 22:50:58 +0900

    Add files via upload

---
+++ 30033.cpp
@@ -0,0 +1,21 @@
+// Rust Study
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, i, cnt = 0;
+	cin >> N;
+	int* A = new int[N]();
+	int* B = new int[N]();
+
+	for (i = 0; i < N; i++) 
+		cin >> A[i];
+	for (i = 0; i < N; i++)
+		cin >> B[i];
+	for (i = 0; i < N; i++) {
+		if (A[i] <= B[i])
+			cnt++;
+	}
+	cout << cnt;
+	return 0;
+}
