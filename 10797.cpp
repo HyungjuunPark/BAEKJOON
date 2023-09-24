e0a82194ba74dccbe80d6c3b4a83d0dde284aa1e
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2023-09-24 22:49:59 +0900

    Add files via upload

---
+++ 10797.cpp
@@ -0,0 +1,18 @@
+// 10∫Œ¡¶
+#include <iostream>
+using namespace std;
+
+int main() {
+	int day, num[5], i, cnt = 0;
+	cin >> day;
+	for (i = 0; i < 5; i++)
+		cin >> num[i];
+	for (i = 0; i < 5; i++) {
+		if (day == num[i])
+			cnt++;
+	}
+	cout << cnt;
+
+
+	return 0;
+}
