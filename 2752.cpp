b0099e1fd1f400a42f269f03b2c7d150f3e1a9a9
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2023-09-28 23:41:09 +0900

    Add files via upload

---
+++ 2752.cpp
@@ -0,0 +1,33 @@
+// 세수정렬
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n[3], cnt[3] = { 0 }, i, result[3];
+
+	cin >> n[0] >> n[1] >> n[2];
+
+	if (n[0] > n[1])
+		cnt[0]++;
+	else
+		cnt[1]++;
+	if (n[0] > n[2])
+		cnt[0]++;
+	else
+		cnt[2]++;
+	if (n[1] > n[2])
+		cnt[1]++;
+	else
+		cnt[2]++;
+
+	for (i = 0; i < 3; i++) {
+		if (cnt[i] == 0)
+			result[0] = n[i];
+		else if (cnt[i] == 1)
+			result[1] = n[i];
+		else
+			result[2] = n[i];
+	}
+	cout << result[0] << " " << result[1] << " " << result[2];
+	return 0;
+}
