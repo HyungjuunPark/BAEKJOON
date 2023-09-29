0cde0b08a181321ea1527ff29152d028137b1a95
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2023-09-29 23:23:18 +0900

    Add files via upload

---
+++ 10039.cpp
@@ -0,0 +1,19 @@
+// Æò±Õ Á¡¼ö
+#include <iostream>
+using namespace std;
+
+int main() {
+	int score[5], avg = 0, i;
+
+	for (i = 0; i < 5; i++) {
+		cin >> score[i];
+		if (score[i] < 40)
+			score[i] = 40;
+		avg += score[i];
+	}
+	avg /= 5;
+
+	cout << avg;
+
+	return 0;
+}
