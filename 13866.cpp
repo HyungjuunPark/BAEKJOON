aef26a1aa4bfddc737c46afbb8d4b5741ae34d8a
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2023-10-03 21:48:28 +0900

    Add files via upload

---
+++ 13866.cpp
@@ -0,0 +1,17 @@
+// ÆÀ ³ª´©±â
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int a, b, c, d;
+
+	cin >> a >> b >> c >> d;
+
+	if ((a + d) >= (b + c))
+		cout << (a + d) - (b + c);
+	else
+		cout << (b + c) - (a + d);
+
+	return 0;
+}
