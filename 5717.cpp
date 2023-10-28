d78ffd723522f557d693e19943a806a7674200c9
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2023-10-28 21:46:40 +0900

    Add files via upload

---
+++ 5717.cpp
@@ -0,0 +1,16 @@
+// 상근이의 친구들
+#include <iostream>
+using namespace std;
+
+int main() {
+	int M, F;
+
+	while (1) {
+		cin >> M >> F;
+		if ((M == 0) && (F == 0))
+			break;
+		cout << M + F << '\n';
+	}
+
+	return 0;
+}
