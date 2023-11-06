ab82d288dbfd6c21de95677fdbf1cea8bf7a4e48
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2023-11-06 20:38:04 +0900

    Add files via upload

---
+++ 10768.cpp
@@ -0,0 +1,23 @@
+// 특별한 날
+#include <iostream>
+using namespace std;
+
+int main() {
+	int m, d;
+
+	cin >> m >> d;
+
+	if (m > 2)
+		cout << "After";
+	else if (m < 2)
+		cout << "Before";
+	else {
+		if (d == 18)
+			cout << "Special";
+		else if (d > 18)
+			cout << "After";
+		else
+			cout << "Before";
+	}
+	return 0;
+}
