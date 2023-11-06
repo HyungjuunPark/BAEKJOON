243a088915ffe468cb10529a2b3243bcc289cf3a
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2023-11-06 20:25:36 +0900

    Add files via upload

---
+++ 10156.cpp
@@ -0,0 +1,16 @@
+// °úÀÚ
+#include <iostream>
+using namespace std;
+
+int main() {
+	int K, N, M;
+
+	cin >> K >> N >> M;
+	if (K * N < M)
+		cout << 0;
+	else
+		cout << K * N - M;
+
+
+	return 0;
+}
