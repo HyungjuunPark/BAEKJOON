01670cdeb0af2e7150e6029bd669b78f034924ca
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2023-11-10 23:09:09 +0900

    Add files via upload

---
+++ 2530.cpp
@@ -0,0 +1,14 @@
+// 인공지능 시계
+#include <iostream>
+using namespace std;
+
+int main() {
+	int A, B, C, D;
+
+	cin >> A >> B >> C >> D;
+
+	cout << (A + (B + (C + D) / 60) / 60) % 24 << " " <<
+		(B + (C + D) / 60) % 60 << " " << (C + D) % 60;
+
+	return 0;
+}
