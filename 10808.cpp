bff890327d96149afc8e08e32d8162636f06eb8c
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2023-11-21 20:27:08 +0900

    Add files via upload

---
+++ 10808.cpp
@@ -0,0 +1,19 @@
+// ¾ËÆÄºª °³¼ö
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int i, j, cnt[26] = { 0 };
+	string S;
+
+	cin >> S;
+	
+	for (i = 0; i < S.length(); i++)
+		cnt[S[i] - 'a']++;
+
+	for (i = 0; i < 26; i++)
+		cout << cnt[i] << " ";
+
+	return 0;
+}
