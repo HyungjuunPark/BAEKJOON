12fbc5725176bc5829d7e8c11f7d13a88d130742
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2023-12-05 23:21:03 +0900

    Add files via upload

---
+++ 5532.cpp
@@ -0,0 +1,29 @@
+// ¹æÇÐ ¼÷Á¦
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int L, A, B, C, D;
+	cin >> L >> A >> B >> C >> D;
+	
+	int hw, mat, kor;
+
+	if (A % C > 0)
+		mat = A / C + 1;
+	else
+		mat = A / C;
+
+	if (B % D > 0)
+		kor = B / D + 1;
+	else
+		kor = B / D;
+
+	hw = mat >= kor ? mat : kor;
+
+	L -= hw;
+
+	cout << L;
+
+	return 0;
+}
