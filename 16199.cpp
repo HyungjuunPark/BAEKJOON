807b4d2f2fc1dfd889b274c69065483621325925
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2023-12-26 22:05:49 +0900

    Add files via upload

--- 16199.cpp
+++ 16199.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 int main() {
 
-	int i, B[3], S[3], M_age = 0, S_age = 1, Y_age=1;
+	int B[3], S[3], M_age, S_age, Y_age;
 
 	cin >> B[0] >> B[1] >> B[2];
 	cin >> S[0] >> S[1] >> S[2];
 
-	if (S[0] - B[0] == 0) {
-		if (S[1] > B[0])
-			M_age++;
-		else if (S[2] > B[2])
-			M_age++;
-	}
-	else {
-		M_age += S[0] - B[0];
-	}
+	if (B[1] < S[1])
+		M_age = S[0] - B[0];
+	else if (B[1] == S[1] && B[2] <= S[2])
+		M_age = S[0] - B[0];
+	else
+		M_age = S[0] - B[0] - 1;
+
+	S_age = S[0] - B[0] + 1;
+	Y_age = S[0] - B[0];
 
 	cout << M_age << '\n' << S_age << '\n' << Y_age;
 
