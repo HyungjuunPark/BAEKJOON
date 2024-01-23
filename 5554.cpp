6b00017d92f57a6ed5927190cd55af1aabda8ee8
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2024-01-23 23:14:12 +0900

    Update 5554.cpp

--- 5554.cpp
+++ 5554.cpp
@@ -1,10 +1,9 @@
-// ½ÉºÎ¸§ °¡´Â ±æ
+// Â½Ã‰ÂºÃŽÂ¸Â§ Â°Â¡Â´Ã‚ Â±Ã¦
 #include <iostream>
 using namespace std;
 
 int main() {
 	int t[4], x, y, i, sum = 0;
-
 	for (i = 0; i < 4; i++) {
 		cin >> t[i];
 		sum += t[i];
