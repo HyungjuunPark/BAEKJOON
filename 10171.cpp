fc8f3bb1e7224e11abc876321f132cbeac94a35a
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2024-02-06 23:17:55 +0900

    Add files via upload

---
+++ 10171.cpp
@@ -0,0 +1,15 @@
+// Hello World
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cout << '\\' << "    " << '/' << '\\' << '\n';
+    cout << " " << ')' << "  " << "( ')" << '\n';
+    cout << '(' << "  /  " << ')' << '\n';
+    cout << " \\(__)|";
+
+    return 0;
+}
