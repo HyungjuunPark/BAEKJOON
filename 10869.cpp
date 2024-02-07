e65e6dea728f958bbd72f924b915c828f27993f7
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2024-02-07 22:45:42 +0900

    Add files via upload

--- 10869.cpp
+++ 10869.cpp
@@ -1,4 +1,4 @@
-// 사칙연산
+// 시험 성적
 #include <iostream>
 using namespace std;
 
@@ -6,14 +6,19 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int a, b;
-    cin >> a >> b;
+    int n;
+    cin >> n;
 
-    cout << a + b << '\n';
-    cout << a - b << '\n';
-    cout << a * b << '\n';
-    cout << a / b << '\n';
-    cout << a % b << '\n';
+    if (100 >= n && n >= 90)
+        cout << "A";
+    else if (89 >= n && n >= 80)
+        cout << "B";
+    else if (79 >= n && n >= 70)
+        cout << "C";
+    else if (69 >= n && n >= 60)
+        cout << "D";
+    else
+        cout << "F";
 
     return 0;
 }
