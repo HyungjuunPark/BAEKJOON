f8f88ab124d241e19e17b14b16721ebb93a89e37
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2024-02-07 22:50:18 +0900

    Add files via upload

---
+++ 2753.cpp
@@ -0,0 +1,20 @@
+// 시험 성적
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int y;
+    cin >> y;
+
+    if (y % 400 == 0)
+        cout << 1;
+    else if (y % 4 == 0 && y % 100 != 0)
+        cout << 1;
+    else
+        cout << 0;
+   
+    return 0;
+}
