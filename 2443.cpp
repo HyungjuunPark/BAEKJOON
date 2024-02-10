fe4cebc7aff011853cb965f72e0d4881cbc9518f
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2024-02-10 23:45:05 +0900

    Add files via upload

---
+++ 2443.cpp
@@ -0,0 +1,22 @@
+// º° Âï±â - 6
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
+            if (j < i)
+                cout << ' ';
+        for (int k = 0; k < (n - i) * 2 - 1; k++)
+            cout << '*';
+        cout << '\n';
+    }
+       
+    return 0;
+}
