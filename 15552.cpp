7a228b3932e19537ef0a7b33adaf2ce43cbd47af
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2024-02-18 23:10:36 +0900

    Add files via upload

---
+++ 15552.cpp
@@ -0,0 +1,20 @@
+// ºü¸¥ A+B
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    for (int t = 0; t < T; t++) {
+        int A, B;
+        cin >> A >> B;
+
+        cout << A + B << "\n";
+    }
+    
+    return 0;
+}
