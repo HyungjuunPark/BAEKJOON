7ce8b02758959c3b5827584ad58fd7d51ab3a6ef
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2024-06-24 22:09:23 +0900

    Add files via upload

---
+++ 1476.cpp
@@ -0,0 +1,34 @@
+// 날짜 계산
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int E, S, M;
+    cin >> E >> S >> M;
+
+    int year = 1;
+    int e = 1, s = 1, m = 1;
+
+    while (true) {
+        if (e == E && s == S && m == M) {
+            break;
+        }
+
+        e++;
+        s++;
+        m++;
+        year++;
+
+        if (e > 15) e = 1;
+        if (s > 28) s = 1;
+        if (m > 19) m = 1;
+    }
+
+    cout << year << endl;
+    return 0;
+
+}
