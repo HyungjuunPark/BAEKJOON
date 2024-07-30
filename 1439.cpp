ec33599125b7d7494dd4e4c97ae6c445db3e457a
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2024-07-30 20:30:33 +0900

    Add files via upload

---
+++ 1439.cpp
@@ -0,0 +1,20 @@
+// µÚÁý±â
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string s;
+    int result = 0;
+    cin >> s;
+
+    for (int i = 0; i < s.length(); i++)
+        if (s[i] != s[i + 1])
+            result++;
+
+    if (!result)
+        cout << result;
+    else cout << result / 2;
+    return 0;
+}
