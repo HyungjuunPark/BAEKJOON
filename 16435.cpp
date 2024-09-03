0892bcdcc6ccc0182c8db088e4dee56944b50e22
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2024-09-03 22:27:16 +0900

    Add files via upload

---
+++ 16435.cpp
@@ -0,0 +1,27 @@
+// 스네이크버드
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N, L, h[10000], cnt = 0;
+    cin >> N >> L;
+   
+    for (int i = 0; i < N; i++)
+        cin >> h[i];
+
+    sort(h, h + N);
+    
+    for (int i = 0; i < N; i++) {
+        if (L < h[cnt])
+            break;
+        else {
+            L++;
+            cnt++;
+        }
+    }
+
+    cout << L;
+
+    return 0;
+}
