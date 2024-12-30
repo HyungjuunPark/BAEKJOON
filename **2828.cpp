6c64b4972ae3b6f03be1ca46a00f37156b9dfa7c
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2024-12-31 00:58:38 +0900

    Update **2828.cpp

--- **2828.cpp
+++ **2828.cpp
@@ -1,22 +1,43 @@
 // 사과 담기 게임
-#include<iostream>
+#include <iostream>
 using namespace std;
+int boxPosLeft, boxPosRight;
 
 int main() {
-    int N, M, j, n[10] = { 0 }, num, l = 0;
-    cin >> N >> M >> j;
-    for (int i = 0; i < j; i++) {
-        cin >> num;
-        n[num]++;
-    }
+    int N, M, J;
+    cin >> N >> M >> J;
+    int move = 0;
+    boxPosLeft = 1;
+    boxPosRight = M;
 
-    while (1) {
-        for (int i = 0; i < M; i++) {
-            if (n[i] != 0)
-                n[i]--;
+    bool apple[10] = { false };  // 사과의 위치를 기록할 배열
+
+    for (int i = 0; i < J; i++) {
+        int applePos;
+        cin >> applePos;
+
+        // 사과 위치 기록
+        apple[applePos - 1] = true;
+
+        // 사과가 있을 경우 처리
+        while (apple[applePos - 1]) {
+            if (boxPosLeft <= applePos && applePos <= boxPosRight) {
+                apple[applePos - 1] = false;  // 사과를 먹은 경우
+            }
+            else {
+                if (applePos < boxPosLeft) {
+                    boxPosLeft -= 1;
+                    boxPosRight -= 1;
+                    move++;
+                }
+                else {
+                    boxPosLeft += 1;
+                    boxPosRight += 1;
+                    move++;
+                }
+            }
         }
     }
 
-    return 0;
+    cout << move << '\n';
 }
-
